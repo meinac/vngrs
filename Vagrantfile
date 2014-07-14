57dff9ae4f25da4eb00f4c4744fff793d39f8a11
Mehmet Emin İNAÇ <mehmetytuce@gmail.com>
2014-07-14 14:57:12 +0300

    vagrant initialized

---
+++ Vagrantfile
@@ -0,0 +1,15 @@
+Vagrant.configure('2') do |config|
+  config.vm.box      = 'precise32'
+  config.vm.box_url  = 'http://files.vagrantup.com/precise32.box'
+  config.vm.hostname = 'vngrs'
+
+  config.vm.network :private_network, ip: "33.33.33.33"
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = 'puppet/manifests'
+    puppet.module_path    = 'puppet/modules'
+  end
+  config.vm.synced_folder ".", "/vagrant", type: "nfs"
+end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,122 @@
+$ar_databases = ['vngrs_development', 'vngrs_test']
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+
+# --- PostgreSQL ---------------------------------------------------------------
+
+class install_postgres {
+  class { 'postgresql': }
+
+  class { 'postgresql::server': }
+
+  pg_user { 'vngrs':
+    superuser => true,
+    ensure  => present,
+    password   => '123',
+    createdb   => true,
+    require => Class['postgresql::server']
+  }
+
+  pg_database { $ar_databases:
+    ensure   => present,
+    encoding => 'UTF8',
+    require  => Class['postgresql::server']
+  }
+
+  pg_user { 'vagrant':
+    ensure    => present,
+    superuser => true,
+    require   => Class['postgresql::server']
+  }
+
+  package { 'libpq-dev':
+    ensure => installed
+  }
+
+  package { 'postgresql-contrib':
+    ensure  => installed,
+    require => Class['postgresql::server'],
+  }
+}
+class { 'install_postgres': }
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
+
+# ImageMagick
+package { ['imagemagick', 'libmagickcore-dev']:
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm/bin/rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # Thanks to @mpapis for this tip.
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0-p195 --latest-binary --autolibs=enabled'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
+
+exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
+  creates => "${home}/.rvm/bin/bundle",
+  require => Exec['install_ruby']
+}
+
+
+# --- Redis Server
+
+exec { 'install_redis':
+  command => "${as_vagrant} 'wget http://download.redis.io/releases/redis-2.8.9.tar.gz && tar xzf redis-2.8.9.tar.gz && cd redis-2.8.9/src && make && cd .. && sudo make install'",
+  creates => "/usr/local/bin/redis-server"
+}
